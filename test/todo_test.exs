429fe91a989a9534a324c332e0dc1f06ff16e3f0
Santiago Gonzalez <santiago.gonzalez@mimiquate.com>
2026-02-12 16:21:39 -0300

    feat(chapter-7): Managing multiple to-do lists, persisting data and writing some tests.

--- test/todo_test.exs
+++ test/todo_test.exs
@@ -1,8 +1,8 @@
 defmodule TodoTest do
-  use ExUnit.Case
-  doctest Todo
+  # use ExUnit.Case
+  # doctest Todo
 
-  test "greets the world" do
-    assert Todo.hello() == :world
-  end
+  # test "greets the world" do
+  #   assert Todo.hello() == :world
+  # end
 end
